18e703356ff5ff13ba4ba12a093661dbc9c0e360
amir-aein <aien0725@gmail.com>
2022-05-29 15:08:27 -0400

    aa

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,7 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 git init
+git checkout -b main
 git add -A
 git commit -m 'deploy'
 git push -f git@github.com:amir-aein/unvention.git master:gh-pages
--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,4 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  base: "git@github.com:amir-aein/unvention.git",
 });
