46a94415fe0b97b62c82d4ddbaca23453d8d2171
amir-aein <aien0725@gmail.com>
2022-05-29 14:53:08 -0400

    foo

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,6 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  publicPath: process.env.NODE_ENV === "production" ? "/unvention/" : "/",
 });
 
